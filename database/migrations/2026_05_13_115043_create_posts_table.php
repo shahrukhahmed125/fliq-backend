52f5a3f44680c2db927341e3813b7103b559e19d
shahrukhahmed125 <shahrukhahmed125@gmail.com>
2026-05-13 17:39:15 +0500

    migrations and models

---
+++ database/migrations/2026_05_13_115043_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->text('content')->nullable();
+            $table->json('media')->nullable();
+            $table->foreignId('parent_id')->nullable()->constrained('posts')->nullOnDelete();
+            $table->foreignId('repost_of')->nullable()->constrained('posts')->nullOnDelete();
+            $table->boolean('is_repost')->default(false);
+            $table->timestamps();
+            $table->softDeletes();
+            $table->index(['user_id', 'created_at']);
+            $table->index('parent_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
---
+++ database/migrations/2026_05_13_115309_create_shares_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('shares', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('post_id')
+                ->constrained()
+                ->cascadeOnDelete();
+
+            $table->foreignId('user_id')
+                ->nullable()
+                ->constrained()
+                ->nullOnDelete();
+
+            // where shared to
+            $table->string('platform')->nullable(); 
+            // e.g. whatsapp, facebook, copy_link, internal share, etc.
+            $table->timestamps();
+            $table->index('post_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('shares');
+    }
+};
